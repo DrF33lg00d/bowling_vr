92b24cd2769357ac8f14149a3bc709d7fc6463e1
DrF33lg00d <7drf33lg00d7@gmail.com>
2021-04-29 20:41:19 +1000

    Add Start scene and main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button startGame;
+    [SerializeField] private Button startTest;
+    [SerializeField] private Button exitGame;
+    [SerializeField] private Slider soundSlider;
+
+    public void LoadTestScene()
+    {
+        SceneManager.LoadScene("Test");
+    }
+
+    public void ExitFromGame()
+    {
+        Debug.Log("Exit from BowlingVR");
+        // Stop Editor if starts in Editor
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+
+    public void ChangeBackSound(Slider sliderItem)
+    {
+        Debug.Log("Changed sound on " + sliderItem.value);
+    }
+}
